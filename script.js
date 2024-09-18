c75d3312c419606f9cb6697d4864454434145b81
Caio Duque <caioduque.dev@gmail.com>
2024-09-18 14:32:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeBackgroundColor() {
+    if(document.getElementById('box').style.backgroundColor == 'black' ) {
+        document.getElementById('box').style.backgroundColor = 'orange'
+    } else {
+        document.getElementById('box').style.backgroundColor = 'black'
+    }
+}
